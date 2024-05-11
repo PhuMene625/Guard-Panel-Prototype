df9183f2824bc9d46b44ff2bb3c0e4d28d4d47c1
PhuMene <79933342+PhuMene625@users.noreply.github.com>
2024-05-11 06:21:54 +0000

    0.2
    On a ajouté le voyant d'interlock et il est fonctionnel.
    Les boutons ajoutés ne fonctionnent pas encore dans le sens logique. Ce ne sont juste que des boutons et voyants qui marchent.
    
    + Voyant Door Interlock
    ~ Platine (base) remplacée pour raison d'une erreur de conception. (Guard panel key switch manquant)

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const leverSwitch = document.querySelector('.lever-switch');
 leverSwitch.addEventListener('click', () => {
   leverSwitch.classList.toggle('on');
 });
+
+function online(){
+  const interlockSwitch = document.querySelector('#ioff');
+  interlockSwitch.classList.toggle('on');
+}
